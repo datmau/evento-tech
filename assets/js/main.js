69359f6034da801bbc439f9443ebe6aca69dc906
Andrés Salazar <andres.salazar.d.18@gmail.com>
2025-04-28 20:45:24 -0500

    comments section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('.form-grid');
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const nombre = form.querySelector("#first-name").value;
-        const interest = form.querySelector("#interest").value;
-        const email = form.querySelector("#email").value;
-        const phone = form.querySelector("#phone-input").value;
-        
-        if(nombre && interest && email && phone){
-            const message = saveRegistration({name: nombre, category: interest, email, phone});
-            alert(message);
-            form.reset();
-        }else{
-            alert('Por favor, completa todos los campos.');
-        }
-       
-    });
+  const form = document.querySelector('.form-grid');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const nombre = form.querySelector('#first-name').value;
+    const interest = form.querySelector('#interest').value;
+    const email = form.querySelector('#email').value;
+    const phone = form.querySelector('#phone-input').value;
+    const comments = form.querySelector('#comments').value;
+
+    if (nombre && interest && email && phone) {
+      const message = saveRegistration({
+        name: nombre,
+        category: interest,
+        email,
+        phone,
+      });
+      alert(message);
+      form.reset();
+    } else {
+      alert('Por favor, completa todos los campos.');
+    }
+
+    if (comments) {
+      alert(saveUserComments({ comments }));
+      form.reset();
+    }
+  });
 });
 
-
-
-console.log(createRegistrationMessage({name: 'Juan', category: 'Estudiante'}));
+console.log(
+  createRegistrationMessage({ name: 'Juan', category: 'Estudiante' })
+);
--- assets/js/registro.js
+++ assets/js/registro.js
@@ -1,7 +1,11 @@
-function createRegistrationMessage({name, category}) {
+function createRegistrationMessage({ name, category }) {
   return `Estimado ${name}, tu registro en la categoría ${category} a sido exitoso.`;
 }
 
-function saveRegistration({name, category, email, phone}) {
+function saveRegistration({ name, category, email, phone }) {
   return `Registro guardado: ${name}, ${category}, ${email}, ${phone}`;
+}
+
+function saveUserComments({ comments }) {
+  return `Tus comentarios ${comments} han sido guardados`;
 }
